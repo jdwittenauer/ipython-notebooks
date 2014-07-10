1c3262c8f4511a9032f987d6a8641249f425ac42
John Wittenauer <jdwittenauer@gmail.com>
2014-07-09 21:22:53 -0400

    Finished advanced libraries notebook.

---
+++ scripts/startup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Start-up script for interactive data science in Python.
+"""
+
+import numpy
+import numpy as np
+import scipy
+import scipy as sp
+import matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import matplotlib.mlab as mlab
+import pandas as pd
+import patsy.damatrices as dm
+import statsmodels.api as sm
+import sklearn as skl
+
+from numpy import *
+from scipy import *
+from pyplot import *
+from pandas import *
+from statsmodels.api import *
+from sklearn import *
