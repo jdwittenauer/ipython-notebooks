5b5c31edbadd1dd3ade450633241613883390cde
John Wittenauer <jdwittenauer@gmail.com>
2014-12-25 21:47:51 -0500

    Updating startup script.

--- scripts/startup.py
+++ scripts/startup.py
@@ -3,6 +3,10 @@
 Start-up script for interactive data science in Python.
 """
 
+# Use '%matplotlib inline' and '%matplotlib qt' at the IPython console 
+# to switch between display modes
+
+# base libraries
 import os
 import random
 import string
@@ -10,22 +14,39 @@ import math
 import time
 import csv
 import pickle
+
+# these come with the Anaconda distribution
 import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sb
 import pandas as pd
 import statsmodels.api as sm
 import sklearn as skl
-
+import nltk
 from numpy import *
 from scipy import *
 from matplotlib import *
 from matplotlib.pyplot import *
-from seaborn import *
 from pandas import *
 from statsmodels.api import *
 from sklearn import *
-
+from nltk import *
 from patsy import dmatrices
+from bs4 import BeautifulSoup
+
+# these are installed separately with conda or pip
+import gensim as gn
+import seaborn as sb
+from gensim import *
+from seaborn import *
+
+# these are installed separately and only run on Linux
+# import pymc
+# from pymc import *
+
+# these are built directly from source and only run on Linux
+# import xgboost as xgb
+# import pylearn2 as pl2
+# from xgboost import *
+# from pylearn2 import *
